e8ed0ca74a10212e50e40cb49f54dc8d24c57302
MohamedKhaled <work.mohamedkhaled@gmail.com>
2026-07-04 12:26:12 +0300

    Add project files.

---
+++ LeaveManagementSystem/Enums/LeaveStatues.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeaveManagementSystem.Enums
+{
+    public enum LeaveStatues
+    {
+        Pending, Approved, Rejected
+    }
+}
